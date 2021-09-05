59038fee1a752d7a68fb4bf43c7c4477b315856e
sdkdeepa <deepa94086@gmail.com>
2021-09-05 01:58:09 -0700

    useState function with states described in app.js

--- react-state-app/src/index.js
+++ react-state-app/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+//Destructing arrays 
+const [, , light] = ['boots', 'tent', 'headlamp']
+console.log(light)
+
 ReactDOM.render(
   <App authorized={true} />,
   document.getElementById('root')
